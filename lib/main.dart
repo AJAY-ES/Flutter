ec3a22e87dbea5473326d236506360141c0fe9e5
AJAY-ES <esajay96@gmail.com>
2020-10-18 10:39:28 +0530

    Execise1

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() =>
+    runApp(Center(child: Text('data', textDirection: TextDirection.ltr)));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
